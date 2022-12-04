1c0a368310710e8ae0bf94b0337add17ef005dd1
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-12-04 13:24:41 +1000

    reset

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,9 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
-const searchTerm = ref('')
-const findProducts = async term => {}
-watch(searchTerm, newTerm => findProducts(newTerm))
+// fetch('https://v2.jokeapi.dev/joke/christmas') 
 </script>
 
 <template>
-  <div class="w-full h-full flex flex-col gap-5 justify-center items-center">
-    <h1 class="text-4xl font-bold">Gift Search Bar</h1>
-    <input type="text" class="p-2 border-2 border-gray-dark" v-model="searchTerm" placeholder="Start typing..." />
-    <ul class="list-disc">
-      <li>Display suggestions here</li>
-    </ul>
+  <div class="w-full h-full flex justify-center items-center">
+    <div>Good luck!</div>
   </div>
 </template>
