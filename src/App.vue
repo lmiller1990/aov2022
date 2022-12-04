f40424eea6878b5e98998bfabb309af2bd52cef2
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-12-04 13:51:44 +1000

    done

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,47 @@
 <script setup lang="ts">
-// fetch('https://v2.jokeapi.dev/joke/christmas') 
+import { ref } from 'vue';
+
+interface Joke {
+  setup: string
+  delivery: string
+}
+
+const joke = ref<Joke>()
+const show = ref(false)
+
+async function fetchJoke () {
+  const res = await window.fetch('https://v2.jokeapi.dev/joke/christmas') 
+  const json = await res.json()
+  return json as Joke
+}
+
+async function getJoke () {
+  const data = await fetchJoke()
+  joke.value = data
+}
+
+function handleShow () {
+  show.value = true
+}
+
+function handleAnother () {
+  joke.value = undefined
+  show.value = false
+  getJoke()
+}
+
+getJoke()
 </script>
 
 <template>
   <div class="w-full h-full flex justify-center items-center">
-    <div>Good luck!</div>
+    <div v-if="joke">
+      <div>{{ joke.setup }}</div>
+      <template v-if="show">
+        <div>{{ joke.delivery }}</div>
+        <button @click="handleAnother">Another</button>
+      </template>
+      <button v-else @click="handleShow">Tell Me!</button>
+    </div>
   </div>
 </template>
