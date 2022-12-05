a0a0b3141b34700cefbf2c451e22039acbf3b200
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-12-05 21:19:06 +1000

    update

--- src/ChristmasTree.vue
+++ src/ChristmasTree.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts">
+<script setup lang="tsx">
+import { FunctionalComponent } from 'vue'
+
 const props = withDefaults(
   defineProps<{
     size: number
@@ -7,10 +9,16 @@ const props = withDefaults(
     size: 1,
   }
 )
+
+const Ball: FunctionalComponent = props => {
+  return <div class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
+}
 </script>
 
 <template>
   <div>
-    <div class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
+    <div v-for="i in size" class="flex justify-center">
+      <Ball v-for="j in i" />
+    </div>
   </div>
 </template>
