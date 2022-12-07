6a94f087c159e7e620713fb310fbc97899811295
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-12-07 18:48:14 +1000

    add select component

--- src/ItemSelect.vue
+++ src/ItemSelect.vue
@@ -1,11 +1,30 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useItemComparison } from './itemComparison'
-const selected = ref()
+import { Item } from './fetchItems'
+
+const props = defineProps<{
+  items: Item[]
+  modelValue?: Item
+}>()
+
+const emits = defineEmits<{
+  (event: 'update:modelValue', item: Item): void
+}>()
+
+function handleChange(event: Event) {
+  const val = parseInt((event.target as HTMLSelectElement).value, 10)
+  emits('update:modelValue', props.items.find(x => x.id === val)!)
+}
 </script>
 
 <template>
-  <select v-model="selected" class="p-2 border-2 border-gray-dark">
-    <option disabled value="">Select an item</option>
+  {{ props.modelValue}}
+  <select @change="handleChange" class="p-2 border-2 border-gray-dark">
+    <option selected disabled value="">Select an item</option>
+    <option 
+      v-for="item in items" 
+      :selected="props.modelValue && item.id === props.modelValue?.id" 
+      :value="item.id">
+      {{ item.title }} - ${{ item.price }}
+    </option>
   </select>
 </template>
