6e82e0df5f70d7304e046fb707f6c1f462d265fc
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-12-12 19:35:43 +1000

    done;

--- src/ChristmasTree.vue
+++ src/ChristmasTree.vue
@@ -10,8 +10,46 @@ const props = withDefaults(
   }
 )
 
+function random (n: number) {
+  return Math.round(Math.random() * 100) % n
+}
+
 const Ball: FunctionalComponent = props => {
-  return <div class="relative rounded-full bg-green-600 w-16 h-16 -m-2 flex justify-center items-center" />
+  const delay = [
+    `[animation-delay:200ms]`, 
+    `[animation-delay:600ms]`,
+    `[animation-delay:1000ms]`
+  ]
+
+  const posX = [
+    `-ml-6`,
+    `-ml-4`,
+    `-ml-0`,
+    `ml-4`,
+    `ml-6`,
+  ]
+
+  const posY = [
+    `-mt-6`,
+    `-mt-4`,
+    `-mt-0`,
+    `mt-4`,
+    `mt-6`,
+  ]
+
+  return <div class={`
+    relative rounded-full bg-green-600 
+    w-16 h-16 -m-2 
+    flex justify-center items-center`}
+  >
+    <div class={`
+      w-2 h-2 bg-yellow-200 rounded
+      animate-[twinkle_1s_infinite]
+      ${delay[random(delay.length)]}
+      ${posX[random(posX.length)]}
+      ${posY[random(posY.length)]}
+    `} />
+  </div>
 }
 </script>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,17 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,vue}'],
   theme: {
+    extend: {
+      keyframes: {
+        twinkle: {
+          '0%, 100%': {
+            opacity: '1'
+          },
+          '50%': {
+            opacity: '0'
+          },
+        }
+      }
+    }
   },
 }
