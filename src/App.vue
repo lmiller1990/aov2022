faaf36c06de627c4d3a2984f2197a14dc368ce52
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-12-14 21:06:45 +1000

    reset

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-// fetch('https://v2.jokeapi.dev/joke/christmas') 
 </script>
 
 <template>
-  <div class="w-full h-full flex justify-center items-center">
-    <div>Good luck!</div>
+  <div class="">
   </div>
 </template>
