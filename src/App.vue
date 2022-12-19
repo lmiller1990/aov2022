e9d0c20424b3f802e7a09a4dcce19876baacb786
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-12-19 22:00:47 +1000

    christmas-done

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="w-full h-full flex flex-col justify-center items-center text-center gap-12">
     <p v-christmas>Red + Green (default)</p>
     <p v-christmas:red>Red only</p>
-    <p v-christmas:green>Green only</p>
+    <p v-christmas:green.abc.bef>Green only</p>
     <p v-christmas="10">Slower Animation</p>
   </div>
 </template>
--- src/christmas.ts
+++ src/christmas.ts
@@ -1,1 +1,16 @@
 import { Directive } from "vue";
+
+export const christmas: Directive<HTMLElement, number> = {
+  created (el, binding) {
+
+    if (binding.arg) {
+      el.classList.add(binding.arg)
+    } else {
+      el.classList.add('red-green')
+    }
+
+    el.classList.add('christmas-text')
+
+    el.style.animation = `textclip ${binding.value ?? 2}s ease infinite`
+  },
+}
