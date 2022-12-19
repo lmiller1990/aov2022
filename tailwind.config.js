629f183badc3a16d5d630828652e936f0fa0471a
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-12-19 21:29:57 +1000

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,4 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,vue}'],
-  theme: {
-    extend: {
-      colors: {
-        green: '#42b883',
-        'gray-dark': '#0f172a',
-      },
-    },
-  },
 }
